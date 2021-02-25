c0b06148c6771ab2854bea1d20439db699910951
ckymn <cokyamanmuhammet@gmail.com>
2021-02-25 13:50:15 +0300

    server

--- server/modules/post/router/updatePost.js
+++ server/modules/post/router/updatePost.js
@@ -7,14 +7,26 @@ const joi = require("../../../util/joi");
 // }).options({ stripUnknown: true })
 
 const route = async (req, res) => {
-  let { body, params , locale} = req;
-  const {_id} = body;
-  const _post = await model.findById(_id);
-  if(!_post)
-    return res.status(404).send(locale("user_not_found"));
-  _post.set(body);
-  await _post.save();
-  return res.send(_post);
+    const { id: _id} = req.params;
+    const post = req.body;
+    try {   
+        const updatePost = await model.findByIdAndUpdate(_id, post, { new:true })
+        res.json(updatePost);
+    } catch (error) {
+        res.status(409).json({
+            message: error.message
+        })
+    }
+
+
+    //   let { body, params , locale} = req;
+//   const {_id} = params;
+//   const _post = await model.findById(_id);
+//   if(!_post)
+//     return res.status(404).send(locale("user_not_found"));
+//   _post.set(body);
+//   await _post.save();
+//   return res.json(_post);
 };
 
 module.exports = {
